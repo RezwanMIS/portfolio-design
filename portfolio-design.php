a5da40b2cdb935bbfab38262f8b829220f2456a0
Rezwan Sheikh <139754574+RezwanMIS@users.noreply.github.com>
2024-11-28 12:04:04 +0600

    added my portfolio design 28 Nov 2024
    This is  my  second work portfolio design in HTML & CSS

---
+++ portfolio-design.php
@@ -0,0 +1,268 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+      <!-- CDN font awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- CDN font awesome -->
+
+    <!-- Google fonts link start -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Satisfy&display=swap" rel="stylesheet">
+    <!-- Google fonts link end -->
+    
+    <!-- style link start for mobile -->
+  <link rel="stylesheet" href="style.css.css">
+    <!-- style link start for mobile -->
+    
+    <!-- style link start for desktop -->
+   <link rel="stylesheet" media="screen and (min-width: 992px)" href="desk.css">
+    <!-- style link endfor desktop -->
+
+
+</head>
+<div>
+  <!-- navbar starts from here -->
+    <div id="home" class="navbar">
+      <div class="nav-logo col-2">
+       <img src="picture/logos.jpg" alt="">
+
+      </div>
+      <div class="nav-heading col-4">
+        <h1>Rezwan Sheikh</h1>
+      </div>
+      <div class="navbar-menu col-6">
+        <a href="#home">Home</a>
+        <a href="#about-me-id">About</a>
+        <a href="#skills">Skills</a>
+        <a href="#contact">Contact</a>
+      </div>
+    </div>
+    
+    <!-- navbar end here -->
+
+    <!-- profile start here -->
+<div class="profile">
+  <img class="top-weather-icon" src="picture/weather.png" alt="">
+
+  <div class="profile-desc">
+    <h1>I am a Computer Teacher <br> & <br>Web Devloper</h1>
+    <p>Hi! My name is Rezwan Sheikh. Welcome to my Personal Profile Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, laborum?</p>
+  </div>
+  <img class="bottom-weather-icon" src="picture/weather.png" alt="">
+</div>
+
+    <!-- profile end here -->
+
+
+      <!-- About start here -->
+<div class="about-me" id="about-me-id" >
+  <h2>ABOUT ME</h2>
+  <hr class="dotted-hr">
+<div class="aboutme-image col-6">
+<img src="picture/profile-PC.jpg" alt="">
+</div>
+<div class="aboutme-desc col-6 ">
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, ut?</p>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto nobis atque quasi debitis! Quia, temporibus.</p>
+
+<hr class="about-hr" >
+<p>Name: Rezwan Sheikh</p>
+<hr class="aboutme-hr" >
+<p>Address: Gazipura, Tongi, Gazipur</p>
+<hr class="aboutme-hr" >
+<p>Email: <a href="mailto:rezwanmis1@gmail.com">rezwanmis1@gmail.com</a></p>
+<hr class="aboutme-hr" >
+<p>Phone: <a href="tel:+01960634996">01960634996</a></p>
+<hr class="aboutme-hr" >
+<p><a href="https://github.com/RezwanMIS">https://github.com/RezwanMIS</a></p>
+<hr class="aboutme-hr" >
+
+
+<p>
+  <a href="">
+  <button class="cv-button">Download CV</button>
+  </a>
+</p>
+</div>
+</div>
+<!-- About end here -->
+
+
+<!-- professinal-exp start here -->
+<div class="professinal-exp" id="professinal-exp" >
+
+<!-- worik-exp start here -->
+      <div class="worik-exp col-4">
+        <div class="card">
+      <div class="card-icon">
+          <img class="round-img" src="picture/work.png" alt="">
+      </div>
+
+      <div class="card-titel">
+       <h2>WORK EXPERIENCE</h2>
+      </div>
+   <div class="card-desc">
+   <ol>
+    <li>
+    <strong>
+    Job Experience : 2.9(Years) <br>
+    Assistant Officer Management Information System (MIS) <br></strong>
+    Joining date: (23 January, 2022 - Continuing) <br>
+    Company: M.M. Knitwear Ltd. <br>
+    Location: Konabari, Gazipur <br>
+    </li>
+
+    <br><li>
+    <b><u>Duties/Responsibilities :</u></b></br>
+    (i).Daily Duties: Basically I am working company self-developed garments ERP software system, Technical data input support & problem solving all the Merchandising & Procurement & Commercial team. I create new users id in ERP module, Provide ERP related training & monitor the preparation of user manuals intended as a reference for end users.<br>
+              
+    (ii).Major Responsibilities: Playing role of mid-level Programming (i) Coding, (ii) Understanding the Requirements, (iii) Designing the Software Architecture, (iv) Designing the Database, (v) Preparing Documentation. <br>
+              
+    (iii).Development Platform: (i) HTML5, (ii) CSS3, (iii) JavaScript, (iv) PHP, (v) MySQL Server , (vi) Visual Studio Code, (vii) Chat GPT-3.5 & Open AI, (viii) VNC Viewer. <br>
+                  
+    (iv).My achievement: I have arrange weekly production reviewing meetings recap with all team. I support a large team of 85 members with ERP software related support on a daily basis.
+    </li>
+   </ol>
+   </div>
+  </div>
+  </div>
+<!-- work-exp end here -->
+
+
+<!-- Education-exp start here -->
+  <div class="education col-4">
+        <div class="card">
+      <div class="card-icon">
+  <img class="round-img" src="picture/education.png" alt="">
+      </div>
+
+      <div class="card-titel">
+       <h2>EDUCATION EXPERIENCE</h2>
+      </div>
+   <div class="card-desc">
+   <ol>
+    <li>
+    <strong> Master of Information & Communication Technology (MICT)</strong><br>
+     Institute: National University <br>
+     Pass Year: 2019 <br>
+     Major: Information and Communication Technology (ICT) <br>
+     Result: CGPA 3.25 (Out of 4)<br>
+     Duration: 1 Year<br>
+              <br>
+    </li>
+
+    <br><li>
+    <strong>Master of Business Administration (MBA)</strong><br>
+    Institute: BGMEA University of Fashion & Technology (BUFT) <br>
+    Pass Year: 2020 <br>
+    Major: Apparel Merchandising <br>
+    Result: CGPA 2.81 (Out of 4)<br>
+    Duration: 2 Year<br>
+    </li>
+   </ol>
+   </div>
+  </div>
+  </div>
+<!-- Education-exp end here -->
+
+
+<!-- skills-exp start here -->
+  <div id="skills" class="skills-exp col-4">
+        <div class="card">
+      <div class="card-icon">
+  <img class="round-img" src="picture/skils.png" alt="">
+      </div>
+
+      <div class="card-titel">
+       <h2>SKILLS EXPERIENCE</h2>
+      </div>
+   <div class="card-desc">
+    <div class="skills-container">
+        <ul>
+          <li class="html">HTML</li>
+          <li class="css">CSS</li>
+          <li class="js">JavaScript</li>
+          <li class="php">PHP</li>
+          <li class="msql">MySql</li>
+          
+
+        </ul>
+    </div>
+  
+   </div>
+  </div>
+  </div>
+<!-- skills-exp end here -->
+
+</div>
+<!-- professoinal-exp div end -->
+
+
+
+<!-- Achivement-content start here -->
+<div class="achivement-contact">
+  <div class="achivement col-4">
+        <h2>Achivement</h2>
+        <hr class="dotted-hr" >
+         <p>1. I have arrange weekly production reviewing meetings recap with all team. </p>
+          <p> 2. I support a large team of 85 members with ERP software related support on a daily basis</p>
+    </div>
+
+
+  <div class="contact col-4">
+         <h2> Contact me</h2>
+         <hr class="dotted-hr" >
+      <form action="mailto:rezwanmis1@gmail.com" method="post" enctype="text/plain"  >
+              <p><input type="text" placeholder="Enter your Name" name="Name" ></p>
+               <p><input type="email" placeholder="Enter your Email" name="Email" ></p>
+               <textarea name="message" id="message" cols="30" rows="10" placeholder="Your message Here" ></textarea>
+               <button type="submit" >Send</button>
+               <button type="reset">Reset</button>
+      </form>
+    </div>
+
+
+  <div id="contact" class="contact-link col-4">
+          <h2>More ways to Contact</h2>
+            <hr class="dotted-hr" >
+
+        <div class="media-button">
+          <button class="btn" onclick="window.open('https://github.com/RezwanMIS')" class="request-callback" ><i class="fa fa-github" aria-hidden="true"></i></button>
+          <button class="btn" onclick="window.open('mailto:rezwanmis1@gmail.com')" class="request-callback" ><i class="fa fa-envelope-o" aria-hidden="true"></i></button>
+          <button class="btn" onclick="window.open('https://www.linkedin.com/in/rezwan-sheikh-4b836121')" class="request-callback" ><i class="fa fa-linkedin" aria-hidden="true"></i></button>
+          <button class="btn" onclick="window.open('https://www.facebook.com/sk.rezwanulislam.1')" class="request-callback" ><i class="fa fa-facebook-official" aria-hidden="true"></i></button>
+        </div>
+
+  </div>
+</div>
+<!-- Achivement-content end here -->
+
+
+<!-- Footer start here -->
+<div class="footer">
+  <div class="footer-menu col-4">
+  <a href="#home">Home</a>
+        <a href="#about-me-id">About</a>
+        <a href="#skills">Skills</a>
+        <a href="#contact">Contact</a>
+  </div>
+
+  <div class="footer-desc col-8">
+ Developed by Rezwan Sheikh &copy; 25 Nov 2024. 
+ Email : sheikhrezwan9@gmail.com 
+ Locaton : Gazipura 27, Tongi, Gazipur.
+  </div>
+</div>
+
+
+
+
+<!-- Footer start here -->
+
+</body>
+</html>
